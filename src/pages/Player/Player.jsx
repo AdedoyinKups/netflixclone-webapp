010a7272b2dddcbdeba6a86b2b6384be3e5bdd62
AdedoyinKups <kupoluyiadedoyin@gmail.com>
2024-08-05 10:35:14 +0100

    first commit

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const Player = () => {
+
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const[apiData, setApiData] = useState({
+    name:"",
+    key:"",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTk4ZmRlODBkZGZmMjI4NDE3MDFlNmNjYzk2ZmQ1MiIsIm5iZiI6MTcyMjM1MzA3NC40MjA3ODgsInN1YiI6IjY2YTZiNTA0MmJiMmVmZTlhODExZDIxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ep2BHn9DrRPNEiXNXHaJxc_pa0V5IvjeNUOuk3nx78U'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+
+
+  },[])
+  
+  
+  return (
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='100%'
+      src={`https:/www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default Player
